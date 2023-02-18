bc9a1ca3fb8e3f98716b11959428732d8bec33e8
Fahmi Fahrizal <fahmih16@gmail.com>
2023-02-18 19:47:15 +0700

    Fahmi Fahrizal-admin page dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/page user/home";
+import Dashboard from "./components/pages/page admin/dashboard";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/pages/page admin/dashboard.js
@@ -0,0 +1,18 @@
+import CollapsibleExample from "../../layouts/sidebar/sidebar";
+import BoxDetail from "../../admin component/boxDetail/boxDetail/boxDetail";
+import "./style.css";
+
+
+const Dashboard = () => {
+    return(
+
+     <div className="BD">
+    <CollapsibleExample />
+    <BoxDetail />
+    </div>
+        
+);
+
+}
+
+export default Dashboard
