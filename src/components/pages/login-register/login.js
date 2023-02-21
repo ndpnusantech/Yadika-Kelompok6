1be5ba59d4cebced75e51ecb99261382566f8b04
unknown <lenacahyani82@gmail.com>
2023-02-21 21:21:13 +0700

    Lena - Menambahkan halaman Login

--- src/components/pages/login-register/login.js
+++ src/components/pages/login-register/login.js
@@ -2,19 +2,26 @@ import "./login.css";
 
 const Login = () => {
     const profile = "/assets/Profile User/profile.png";
+
+    const wallpaperLogin = {
+      height: "100vh",
+      backgroundImage: `url('assets/background/Frame 1.png')`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover"
+  }
   
     return(
-     <div className="login">
-      <form action="">
+     <div className="login" style={wallpaperLogin}>
+      <form>
         <h1>Sign In</h1>
         <div className="pro">
-        <img src={profile} alt="" />
+        <img src={profile} alt="profile" />
         </div>
         <input type="text" placeholder="Email/Username" id="" />
         <input type="password" placeholder="password" id="" />
         <div className="link">
-        <a href="http://">Forgot Password</a> <br />
-        <a href="http://">dont have an account? Sign up</a>
+        <a href="/forgot">Forgot Password</a> <br />
+        <a href="/register">dont have an account? Sign up</a>
         </div>
         <button>Login</button>
       </form>
