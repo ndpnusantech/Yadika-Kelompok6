4588c843fedb701e1c7a826d32a40165e7c309ce
LENACAHYANI <lenacahyani82@gmail.com>
2023-02-28 20:11:44 +0700

    lena faiza rizki al - menambahkan halaman contact

--- src/components/user components/contact/contact.js
+++ src/components/user components/contact/contact.js
@@ -1,12 +1,62 @@
 import "./contact.css";
 
 const Contact = () => {
-    return(
-        <div className="contact">
+
+    const surat = "./assets/contact/surat.svg";
+  const telpon = "./assets/contact/telpon.svg";
+  const gedung = "./assets/contact/gedung.svg";
+  const jam = "./assets/contact/jam.svg";
+
+  return (
+    <div className="contact">
+        <div className="title">
             <h1>Contact Us</h1>
-            <p>Lorem ipsum dolor sit amet, deleniti repellat voluptatem error exercitationem.</p>
         </div>
-    );
+      
+      <p>
+        Lorem ipsum dolor sit amet, deleniti <br></br> repellat voluptatem error
+        exercitationem.
+      </p>
+      <div className="form">
+        <input type="text" placeholder="Name" className="name" /> <br /> <hr />
+        <input type="text" placeholder="Email" className="email" /> <br />{" "}
+        <hr />
+        <input
+          type="text"
+          placeholder="Message"
+          className="message"
+        /> <br /> <hr />
+      </div>
+      <button>Send</button>
+      <div className="box-1"></div>
+      <div className="box-2">
+        <h1 align="left">Info</h1>
+        <table>
+            <tr>
+                <td><img src={surat} alt="" /></td>
+                <td>lenacahyani82@gmail.com</td>
+            </tr>
+            <tr>
+                <td className="pt-5 pe-4"><img src={telpon} alt=""  /></td>
+                <td  className="pt-5">lenacahyani82@gmail.com</td>
+            </tr>
+            <tr>
+                <td  className="pt-5"><img src={gedung} alt="" /></td>
+                <td  className="pt-5">lenacahyani82@gmail.com</td>
+            </tr>
+            <tr>
+                <td  className="pt-5"><img src={jam} alt="" /></td>
+                <td  className="pt-5">lenacahyani82@gmail.com</td>
+            </tr>
+            
+        </table>
+        
+      </div>
+      <div className="box-3">
+        
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
