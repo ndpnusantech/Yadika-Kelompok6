3b8cf6b27040716c5ddbdbd293c7cf828952d846
ridwandahroni <dahroniridwan66@gmail.com>
2023-03-02 01:42:39 +0700

    Ridwan - merapihkan tampilan pesanan saya

--- src/components/user components/pesananSaya/pesananSaya.js
+++ src/components/user components/pesananSaya/pesananSaya.js
@@ -1,15 +1,13 @@
-import { Container } from "react-bootstrap";
+import { Container, Table } from "react-bootstrap";
 import "./pesanan.css";
 const PesananSaya = () => {
-  const arrow = "assets/Icon Sosial Media/arrow-right-circle 1.png";
   return (
     <Container>
       <div className="pesananSaya">
-        <img src={arrow} alt="" />
-        <div class="judul">
-          <h1>Pesanan Saya</h1>
+        <div class="title mb-5 mt-5">
+          <h2 className="fw-semibold">Pesanan Saya</h2>
         </div>
-        <table>
+        <Table bordered className="text-center">
           <thead>
             <tr>
               <th>Id Pesanan</th>
@@ -25,31 +23,31 @@ const PesananSaya = () => {
               <td>Vicks Formula 44 Sirup 100ml</td>
               <td>4</td>
               <td>Rp. 74.600</td>
-              <td class="merah">Menunggu Pembayaran</td>
+              <td class="text-danger fw-semibold">Menunggu Pembayaran</td>
             </tr>
             <tr>
               <td>0998289819892212</td>
               <td>Oskadon Biru</td>
               <td>2</td>
               <td>Rp.8.000</td>
-              <td>Diproses Penjual</td>
+              <td className="text-secondary fw-semibold">Diproses Penjual</td>
             </tr>
             <tr>
               <td>8989992300892545</td>
               <td>Paramek</td>
               <td>1</td>
               <td>Rp1.000</td>
-              <td class="hijau">Sedang dikirim</td>
+              <td class="text-success-emphasis fw-semibold">Sedang dikirim</td>
             </tr>
             <tr>
               <td>7878574890023028</td>
               <td>Insa</td>
               <td>4</td>
               <td>Rp.10.000</td>
-              <td class="hijau2">Selesai</td>
+              <td class="text-success fw-semibold">Selesai</td>
             </tr>
           </tbody>
-        </table>
+        </Table>
       </div>
     </Container>
   );
