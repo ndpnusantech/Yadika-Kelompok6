a50a9107b9659194b5a0eecd260f6fbe3e536e46
LENACAHYANI <lenacahyani82@gmail.com>
2023-03-05 08:53:34 +0700

    Lena - Menambahkan halaman DiabetesPage

---
+++ src/pages/page user/diabetes.js
@@ -0,0 +1,14 @@
+import NavigationBar from "../../components/layouts/navbar/navigationBar";
+import Diabetes from "../../components/user components/diabetes/diabetes";
+import Footer from "../../components/layouts/Footer/footer";
+
+const Diabetespage = () => {
+    return (
+        <div className="diabetesPage">
+            <NavigationBar />
+            <Diabetes />
+            {/* <Footer /> */}
+        </div>
+    );
+}
+export default Diabetespage;
