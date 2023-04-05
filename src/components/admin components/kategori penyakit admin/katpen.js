db2d70f1b39a1889d72f54f697d00aec71aa6341
FahmiFH2006 <fahmifh16@gmail.com>
2023-04-05 21:37:29 +0700

    Fahmi-menambahkan tabel dan button di kategori penyakit

--- src/components/admin components/kategori penyakit admin/katpen.js
+++ src/components/admin components/kategori penyakit admin/katpen.js
@@ -1,8 +1,37 @@
-import React from "react";
+import "./style.css";
+import Table from 'react-bootstrap/Table';
 
-const CategoryPenyakit = () => {
+const KatPenya = () => {
   return(
     <>
+    <div className="mainkatpen">
+      <h2>Kategori Penyakit</h2> 
+      <a className="addcateg" href="http://">Add Category</a>
+      <div className="tablekatpen">
+      <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Kategori</th>
+          <th></th>
+          <th>action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td colSpan={2}>Masuk Angin</td>
+          <td><a className="delcattab" href="http://">Delete</a> <a className=" editcattab" href="http://">Edit</a> </td>
+
+        </tr>
+        
+       
+      </tbody>
+    </Table>
+      </div>
+    </div>
     </>
   )
 }
+
+export default KatPenya;
--- src/pages/page admin/penyakitCategory.js
+++ src/pages/page admin/penyakitCategory.js
@@ -1,9 +1,11 @@
 import Sidebar from "../../components/layouts/sidebar/sidebar";
+import KatPenya from "../../components/admin components/kategori penyakit admin/katpen";
 
 const ObatKategoriAdmin = () => {
     return(
         <>
          <Sidebar />
+         <KatPenya />
         </>
     )
 }
