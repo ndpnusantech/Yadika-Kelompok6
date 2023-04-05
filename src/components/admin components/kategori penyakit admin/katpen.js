a635f5d672020de2ca7fecdb6a928d655aadcd88
FahmiFH2006 <fahmifh16@gmail.com>
2023-04-05 22:11:26 +0700

    Fahmi-Menambahkan modal form untuk menambah kategori dan tombol delete kategori

--- src/components/admin components/kategori penyakit admin/katpen.js
+++ src/components/admin components/kategori penyakit admin/katpen.js
@@ -1,14 +1,30 @@
 import "./style.css";
 import Table from 'react-bootstrap/Table';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
 
 const KatPenya = () => {
+  const [show, setShow] = useState(false);
+  const [showedit, setShowedit] = useState(false);      
+
+  const handleClosedit = () => setShowedit(false);
+  const handleShowdit = () => setShowedit(true);
+  
+
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return(
     <>
     <div className="mainkatpen">
       <h2>Kategori Penyakit</h2> 
-      <a className="addcateg" href="http://">Add Category</a>
+      <a className="addcateg" onClick={handleShow}>Add Category</a>
       <div className="tablekatpen">
-      <Table striped bordered hover variant="dark">
+      <Table striped bordered hover variant="dark" >
       <thead>
         <tr>
           <th>#</th>
@@ -21,13 +37,84 @@ const KatPenya = () => {
         <tr>
           <td>1</td>
           <td colSpan={2}>Masuk Angin</td>
-          <td><a className="delcattab" href="http://">Delete</a> <a className=" editcattab" href="http://">Edit</a> </td>
-
+          <td><a className="delcattab"  >Delete</a>  </td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td colSpan={2}>Sakit Gigi</td>
+          <td><a className="delcattab"  >Delete</a>  </td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td colSpan={2}>Batuk & Pilek</td>
+          <td><a className="delcattab"  >Delete</a>  </td>
         </tr>
         
-       
+       {/* <a className=" editcattab"onClick={handleShowdit} >Edit</a> */}
+
       </tbody>
     </Table>
+    <div className="modal">
+    <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Nama Kategori</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder=""
+                autoFocus
+              />
+            </Form.Group>
+            
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showedit} onHide={handleClosedit}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClosedit}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClosedit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      </div>
       </div>
     </div>
     </>
